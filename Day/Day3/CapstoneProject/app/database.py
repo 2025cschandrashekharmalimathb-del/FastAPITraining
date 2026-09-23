59899cc552a4874fda7c50021844b5bd36f1ab7d
2025cschandrashekharmalimathb-del <2025cs_chandrashekharmalimath_b@nie.ac.in>
2026-09-23 12:59:31 +0530

    CP 1

---
+++ Day/Day3/CapstoneProject/app/database.py
@@ -0,0 +1,17 @@
+#this file creates a single,shared connection to MongoDB using pymongo
+from pymongo import MongoClient
+from pymongo.database import Database
+
+from app.config import settings
+
+#mongoclient manages a pool of connections to mongodb server.
+client: MongoClient = MongoClient(settings.MONGO_URI)
+database: Database= client[settings.MONGO_DB_NAME]
+
+#sends a ping command to mongodb to confirm the connection is alive.
+def ping_database() -> bool:
+    try:
+        client.admin.command("ping")
+        return True
+    except Exception:
+        return False
